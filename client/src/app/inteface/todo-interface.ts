af908d5248994a103fd130de7ac28919a9cc4c18
Aina261 <36484918+Aina261@users.noreply.github.com>
2019-01-23 13:09:04 +0100

    Revert "Client"

--- client/src/app/inteface/todo-interface.ts
+++
@@ -1,7 +0,0 @@
-export interface TodoInterface {
-    _id: String;
-    name: String;
-    createdDate: Date;
-    due_date: Date;
-    completed: Boolean
-}
--- server/routes/todoRoutes.js
+++
@@ -1,9 +0,0 @@
-todoController = require('../controllers/todoController');
-
-module.exports = (server) => {
-    server.get('/todo', todoController.getTodos);
-    server.get('/todo/:id', todoController.getTodo);
-    server.post('/todo', todoController.createTodo);
-    server.put('/todo/:id', todoController.updateTodo);
-    server.delete('/todo/:id', todoController.deleteTodo);
-};
